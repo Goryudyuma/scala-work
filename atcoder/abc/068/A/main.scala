6c58af6d901a47115dc29eb1e4c13d6ac655006e
Goryudyuma <umaretekyoumade@gmail.com>
2017-07-31 21:19:22 +0900

    abc068A

---
+++ atcoder/abc/068/A/main.scala
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+object Main {
+
+  def solve(sc: => Scanner): Unit = {
+    println("ABC" + sc.next)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val sc: Scanner = new Scanner(System.in)
+    solve(sc)
+  }
+}
