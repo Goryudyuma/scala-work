41cbbc819845bd5c67b3d042bd21cc52aac06f15
Goryudyuma <umaretekyoumade@gmail.com>
2017-11-19 21:22:19 +0900

    aoj2468

---
+++ aoj/2468/main.scala
@@ -0,0 +1,64 @@
+import java.util.Scanner
+
+import scala.collection.Searching._
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+
+class IUnionFind(val size: Int) {
+
+  private case class Node(var parent: Option[Int], var treeSize: Int)
+
+  private val nodes = Array.fill[Node](size)(new Node(None, 1))
+
+  def union(t1: Int, t2: Int): IUnionFind = {
+    if (t1 == t2) return this
+
+    val root1 = root(t1)
+    val root2 = root(t2)
+    if (root1 == root2) return this
+
+    val node1 = nodes(root1)
+    val node2 = nodes(root2)
+
+    if (node1.treeSize < node2.treeSize) {
+      node1.parent = Some(t2)
+      node2.treeSize += node1.treeSize
+    } else {
+      node2.parent = Some(t1)
+      node1.treeSize += node2.treeSize
+    }
+    this
+  }
+
+  def connected(t1: Int, t2: Int): Boolean = t1 == t2 || root(t1) == root(t2)
+
+  @tailrec
+  private def root(t: Int): Int = nodes(t).parent match {
+    case None => t
+    case Some(p) => root(p)
+  }
+}
+
+
+object Main {
+  def solve(sc: => Scanner): Unit = {
+    val N, M = sc.nextInt
+    val A = Array.fill(N)(sc.nextInt).reverse
+    var B = Set[Int]()
+    var count = 0
+    var idx = 0
+    while (count < M && idx < A.size) {
+      if (!B(A(idx))) {
+        B = B + A(idx)
+        count += 1
+        println(A(idx))
+      }
+      idx += 1
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val sc: Scanner = new Scanner(System.in)
+    solve(sc)
+  }
+}
