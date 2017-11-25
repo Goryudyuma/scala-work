8f83c32de2230604b43f61d52af6f6a4c2441a78
Goryudyuma <umaretekyoumade@gmail.com>
2017-11-25 23:20:09 +0900

    cf17-final-open.contest/A

---
+++ atcoder/other/cf17-final-open.contest/A/main.scala
@@ -0,0 +1,70 @@
+import java.util.Scanner
+
+import scala.collection.Searching._
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+
+class IUnionFind(val size: Int) {
+
+  private case class Node(var parent: Option[Int], var treeSize: Int)
+
+  private val nodes = Array.fill[Node](size)(new Node(None, 1))
+
+  def union(t1: Int, t2: Int): IUnionFind = {
+    if (t1 == t2) return this
+
+    val root1 = root(t1)
+    val root2 = root(t2)
+    if (root1 == root2) return this
+
+    val node1 = nodes(root1)
+    val node2 = nodes(root2)
+
+    if (node1.treeSize < node2.treeSize) {
+      node1.parent = Some(t2)
+      node2.treeSize += node1.treeSize
+    } else {
+      node2.parent = Some(t1)
+      node1.treeSize += node2.treeSize
+    }
+    this
+  }
+
+  def connected(t1: Int, t2: Int): Boolean = t1 == t2 || root(t1) == root(t2)
+
+  @tailrec
+  private def root(t: Int): Int = nodes(t).parent match {
+    case None => t
+    case Some(p) => root(p)
+  }
+}
+
+
+object Main {
+  def solve(sc: => Scanner): Unit = {
+    val s = sc.next
+    val akiba = "AKIHABARA"
+    var i = 0
+    var j = 0
+    while (i < s.length && j < akiba.length) {
+      if (s(i) == akiba(j)) {
+        i += 1
+        j += 1
+      } else if (akiba(j) == 'A') {
+        j += 1
+      } else {
+        j = akiba.length
+      }
+    }
+    if (i == s.length && j >= akiba.length - 1) {
+      println("YES")
+    } else {
+      println("NO")
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val sc: Scanner = new Scanner(System.in)
+    solve(sc)
+  }
+}
