cc2e1d5a5e920e807f531620d81cfb84ec0f69ed
Goryudyuma <umaretekyoumade@gmail.com>
2018-02-07 21:03:26 +0900

    yukicoder/637

---
+++ yukicoder/637/main.scala
@@ -0,0 +1,57 @@
+import java.util.Scanner
+
+import scala.collection.Searching._
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+import scala.io.StdIn.readLine
+
+class IUnionFind(val size: Int) {
+
+  private case class Node(var parent: Option[Int], var treeSize: Int)
+
+  private val nodes = Array.fill[Node](size)(new Node(None, 1))
+
+  def union(t1: Int, t2: Int): IUnionFind = {
+    if (t1 == t2) return this
+
+    val root1 = root(t1)
+    val root2 = root(t2)
+    if (root1 == root2) return this
+
+    val node1 = nodes(root1)
+    val node2 = nodes(root2)
+
+    if (node1.treeSize < node2.treeSize) {
+      node1.parent = Some(t2)
+      node2.treeSize += node1.treeSize
+    } else {
+      node2.parent = Some(t1)
+      node1.treeSize += node2.treeSize
+    }
+    this
+  }
+
+  def connected(t1: Int, t2: Int): Boolean = t1 == t2 || root(t1) == root(t2)
+
+  @tailrec
+  private def root(t: Int): Int = nodes(t).parent match {
+    case None => t
+    case Some(p) => root(p)
+  }
+}
+
+
+object Main {
+  def solve(sc: => Scanner): Unit = {
+    println(Array.fill(5)(sc.nextInt).map(n => (if (n % 15 == 0) ("FizzBuzz") else if (n % 3 == 0) ("Fizz") else if (n % 5 == 0) ("Buzz") else (n.toString)).length).sum)
+  }
+
+  def gcd(i: Int, j: Int): Int = {
+    if (i < j) (gcd(j, i)) else (if (j == 0) (i) else (gcd(j, i % j)))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val sc: Scanner = new Scanner(System.in)
+    solve(sc)
+  }
+}
