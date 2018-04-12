1932b5b13a7e95cd8851567d53c59080f87b6e40
Goryudyuma <umaretekyoumade@gmail.com>
2018-04-12 19:48:23 +0900

    aoj/1356

---
+++ aoj/1356/main.scala
@@ -0,0 +1,68 @@
+import java.util.Scanner
+
+import scala.collection.Searching._
+import scala.annotation.tailrec
+import scala.collection.immutable.{NumericRange, Queue}
+import scala.collection.mutable
+import scala.io.StdIn.readLine
+
+class IUnionFind(val size: Int) {
+
+  private case class Node(var parent: Option[Int], var treeSize: Int)
+
+  private val nodes = Array.fill[Node](size)(new Node(None, 1))
+
+  def union(t1: Int, t2: Int): IUnionFind = {
+    if (t1 == t2) return this
+
+    val root1 = root(t1)
+    val root2 = root(t2)
+    if (root1 == root2) return this
+
+    val node1 = nodes(root1)
+    val node2 = nodes(root2)
+
+    if (node1.treeSize < node2.treeSize) {
+      node1.parent = Some(t2)
+      node2.treeSize += node1.treeSize
+    } else {
+      node2.parent = Some(t1)
+      node1.treeSize += node2.treeSize
+    }
+    this
+  }
+
+  def connected(t1: Int, t2: Int): Boolean = t1 == t2 || root(t1) == root(t2)
+
+  def root(t: Int): Int = nodes(t).parent match {
+    case None => t
+    case Some(p) => root(p)
+  }
+}
+
+
+object Main {
+  def solve(sc: => Scanner): Unit = {
+    val A = Array.fill(sc.nextInt)(sc.nextInt)
+    val B = Array.fill(A.size * (A.size + 1))(-1L)
+    for (i <- Range(0, A.size)) (
+      for (j <- Range(i, math.min(i + 8, A.size))) {
+        var n = 0
+        for (k <- Range(i, j + 1)) {
+          n *= 10
+          n += A(k)
+        }
+        B((A.size - j - 1) * A.size + i) = n
+      })
+    println(B.filter(_ >= 0).toSet.toList.sorted.zipWithIndex.filter(x => x._1 != x._2)(0)._2)
+  }
+
+  def gcd(i: Long, j: Long): Long = {
+    if (i < j) (gcd(j, i)) else (if (j == 0) (i) else (gcd(j, i % j)))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val sc: Scanner = new Scanner(System.in)
+    solve(sc)
+  }
+}
