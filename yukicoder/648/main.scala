7ef43564417da1414171b496a554ac08e0b5c1ea
Goryudyuma <umaretekyoumade@gmail.com>
2018-05-02 18:49:59 +0900

    yukicoder/648

---
+++ yukicoder/648/main.scala
@@ -0,0 +1,78 @@
+import java.util.Scanner
+
+import scala.collection.Searching._
+import scala.annotation.tailrec
+import scala.collection.mutable
+import scala.collection.immutable._
+import scala.io.StdIn.readLine
+
+class IUnionFind(val size: Int) {
+
+  private case class Node(var parent: Option[Int], var treeSize: Int)
+
+  private val nodes = Array.fill[Node](size)(new Node(None, 1))
+
+  def union(t1: Int, t2: Int): IUnionFind = {
+    if (t1 == t2) return this
+
+    val root1 = root(t1)
+    val root2 = root(t2)
+    if (root1 == root2) return this
+
+    val node1 = nodes(root1)
+    val node2 = nodes(root2)
+
+    if (node1.treeSize < node2.treeSize) {
+      node1.parent = Some(t2)
+      node2.treeSize += node1.treeSize
+    } else {
+      node2.parent = Some(t1)
+      node1.treeSize += node2.treeSize
+    }
+    this
+  }
+
+  def connected(t1: Int, t2: Int): Boolean = t1 == t2 || root(t1) == root(t2)
+
+  def root(t: Int): Int = nodes(t).parent match {
+    case None => t
+    case Some(p) => root(p)
+  }
+}
+
+object Main {
+  def solve(sc: => Scanner): Unit = {
+    val N = sc.nextLong
+    val ans = solve2(0, 1L << 32, N)
+    if (ans._1) {
+      println("YES")
+      println(ans._2)
+    } else {
+      println("NO")
+    }
+  }
+
+  def solve2(minV: Long, maxV: Long, N: Long): (Boolean, Long) = {
+    if (minV + 2 > maxV) {
+      (calc(minV) == N, minV)
+    } else {
+      val midV = (minV + maxV) / 2
+      if (calc(midV) <= N) {
+        solve2(midV, maxV, N)
+      } else {
+        solve2(minV, midV, N)
+      }
+    }
+  }
+
+  def calc(v: Long): Long = (v + 1) * v / 2
+
+  def gcd(i: Long, j: Long): Long = {
+    if (i < j) (gcd(j, i)) else (if (j == 0) (i) else (gcd(j, i % j)))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val sc: Scanner = new Scanner(System.in)
+    solve(sc)
+  }
+}
