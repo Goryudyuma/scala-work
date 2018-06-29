8ef92b26c60e31cc8040270106e3d22b73d51506
Goryudyuma <umaretekyoumade@gmail.com>
2018-06-29 18:23:09 +0900

    yukicoder/094

---
+++ yukicoder/094/main.scala
@@ -0,0 +1,170 @@
+import java.util.Scanner
+
+import scala.collection.Searching._
+import scala.annotation.tailrec
+import scala.collection.mutable
+import scala.collection.immutable._
+import scala.io.StdIn.readLine
+
+class IUnionFind(val size: Int) {
+
+  private case class Node(var parent: Option[Int], var treeSize: Int)
+
+  private val nodes = Array.fill[Node](size)(new Node(None, 1))
+
+  def union(t1: Int, t2: Int): IUnionFind = {
+    if (t1 == t2) return this
+
+    val root1 = root(t1)
+    val root2 = root(t2)
+    if (root1 == root2) return this
+
+    val node1 = nodes(root1)
+    val node2 = nodes(root2)
+
+    if (node1.treeSize < node2.treeSize) {
+      node1.parent = Some(t2)
+      node2.treeSize += node1.treeSize
+    } else {
+      node2.parent = Some(t1)
+      node1.treeSize += node2.treeSize
+    }
+    this
+  }
+
+  def connected(t1: Int, t2: Int): Boolean = t1 == t2 || root(t1) == root(t2)
+
+  def root(t: Int): Int = nodes(t).parent match {
+    case None => t
+    case Some(p) => {
+      nodes(t).parent = Some(root(p))
+      nodes(t).parent.get
+    }
+  }
+}
+
+object Main {
+  def solve(sc: => Scanner): Unit = {
+    val N = sc.nextInt
+    if (N == 0) (println(1)) else {
+      val uf = new IUnionFind(N)
+      val A = Array.fill(N)((sc.nextInt, sc.nextInt)).zipWithIndex
+      for (i <- A) {
+        for (j <- A) {
+          if ((i._1._1 - j._1._1) * (i._1._1 - j._1._1) + (i._1._2 - j._1._2) * (i._1._2 - j._1._2) <= 100) {
+            uf.union(i._2, j._2)
+          }
+        }
+      }
+      var ans = 0.0
+      for (i <- A) {
+        for (j <- A) {
+          if (uf.connected(i._2, j._2)) {
+            ans = math.max(ans, math.sqrt((i._1._1 - j._1._1) * (i._1._1 - j._1._1) + (i._1._2 - j._1._2) * (i._1._2 - j._1._2)))
+          }
+        }
+      }
+      println((ans + 2.0).formatted("%.20f"))
+    }
+  }
+
+  def show(i: Int, j: Int, N: Int): String = {
+    return (recursive(i, j, N) + 1).formatted("%03d")
+  }
+
+  def recursive(i: Int, j: Int, N: Int): Int = {
+    if (i == 0) {
+      j
+    } else if (i == N - 1) {
+      (N - 1) * 3 - j
+    } else if (j == 0) {
+      (N - 1) * 4 - i
+    } else if (j == N - 1) {
+      N + i - 1
+    } else {
+      recursive(i - 1, j - 1, N - 2) + (N - 1) * 4
+    }
+  }
+
+  def recursive2(N: Long): String = {
+    if (N == 0) ("") else (('a'.toInt + (N % 26)).toChar + recursive2(N / 26))
+  }
+
+  def check(A: Array[List[Int]]): Boolean = {
+    A.map(_.size == 0).fold(true)((a, b) => a & b)
+  }
+
+  def shift(n: Long): Long = {
+    if (n == 0) (0) else if (n == 1) (1) else (shift(n - 1) << 1)
+  }
+
+  def unShift(n: Long): Long = {
+    if (n == 0) (0) else (unShift(n >> 1) + 1)
+  }
+
+  def gcd(i: Long, j: Long): Long = {
+    if (i < j) (gcd(j, i)) else (if (j == 0) (i) else (gcd(j, i % j)))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val sc: Scanner = new Scanner(System.in)
+    solve(sc)
+  }
+}
+
+object Util {
+  def getPermutation(begin: Long = 0): Stream[Long] =
+    Stream.cons(begin, getPermutation(begin + 1))
+
+  def getPrimeList(): Stream[Long] =
+    getPrimeListRecursive(getPermutation(begin = 2))
+
+  private def getPrimeListRecursive(A: Stream[Long]): Stream[Long] =
+    Stream.cons(A.head, getPrimeListRecursive(A.tail.filter(_ % A.head != 0)))
+
+  def fib(a: Long = 0, b: Long = 1, mod: Long = Long.MaxValue): Stream[Long] = a #:: fib(b % mod, (a + b) % mod, mod)
+}
+
+object ArabicRoman {
+
+  type =?>[A, B] = PartialFunction[A, B]
+
+  val codeTable = List(
+    (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"),
+    (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I"))
+
+  val arabicToRoman: (Int) =?> String = {
+    case src if (src >= 1 && src <= 3999) => {
+
+      def convert(left: Int, cont: String = "", code: List[(Int, String)] = codeTable): String = {
+        val (unitVal, unitChar) = code.head
+        left - unitVal match {
+          case n if (n == 0) => cont + unitChar
+          case n if (n > 0) => convert(n, cont + unitChar, code)
+          case _ => convert(left, cont, code.tail)
+        }
+      }
+
+      convert(src)
+    }
+  }
+
+  val romanToArabic: (String) =?> Int = {
+    case src if (Option(src).exists { s => {
+      s.nonEmpty && ("""[^MDCLXVI]""".r.findFirstMatchIn(s.toUpperCase) == None)
+    }
+    }) => {
+
+      def convert(left: String, cont: Int = 0, code: List[(Int, String)] = codeTable): Int = {
+        val (unitVal, unitChar) = code.head
+        left.splitAt(unitChar.length) match {
+          case ("", _) => cont
+          case (`unitChar`, tail) => convert(tail, cont + unitVal, code)
+          case _ => convert(left, cont, code.tail)
+        }
+      }
+
+      convert(src.toUpperCase())
+    }
+  }
+}
